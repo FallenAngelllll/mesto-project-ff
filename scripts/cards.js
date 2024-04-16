b4d20f9bc6df754cc23c9aab7f9fb035af5981b6
FallenAngelllll <146023380+FallenAngelllll@users.noreply.github.com>
2024-04-16 22:12:01 +0500

    добавлено отображение карточек, кнопка удалить

--- scripts/cards.js
+++ scripts/cards.js
@@ -1,15 +1,15 @@
 const initialCards = [
     {
-      name: "Архыз",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
+      name: "Калининград",
+      link: "https://images.unsplash.com/photo-1629104411440-7ebc9134f93e?q=80&w=3687&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     },
     {
-      name: "Челябинская область",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
+      name: "Сочи",
+      link: "https://images.unsplash.com/photo-1599827206955-018384540f40?q=80&w=3642&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     },
     {
-      name: "Иваново",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
+      name: "Санкт-Петербург",
+      link: "https://images.unsplash.com/photo-1609524400436-2226c04f8a84?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     },
     {
       name: "Камчатка",
--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,28 @@
 // @todo: Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content;
 
 // @todo: DOM узлы
+const container = document.querySelector('.content');
+const places = container.querySelector('.places');
+const cardsContainer = container.querySelector('.places__list');
 
 // @todo: Функция создания карточки
+function addCard({name, link, deleteCard}) {
+    const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
+    const cardImage = cardElement.querySelector('.card__image');
+    cardElement.querySelector('.card__title').textContent = name;
+    cardImage.src = link;
+
+    const deleteButton = cardElement.querySelector('.card__delete-button');
+    deleteButton.addEventListener('click', () => deleteCard(cardElement));
+
+    return cardElement;
+  };
 
 // @todo: Функция удаления карточки
+const deleteCard = (element) => element.remove();
 
 // @todo: Вывести карточки на страницу
+initialCards.forEach(({name, link}) =>
+    cardsContainer.append(addCard({name, link, deleteCard}))
+);
