d0a50cd77e1b1fe206a19580b5ba7156d0cb32d8
Julie Rodionova <behappyeveryday458@gmail.com>
2024-08-02 21:37:17 +0500

    Добавляет сборку

--- src/scripts/cards.js
+++ src/scripts/cards.js
@@ -1,4 +1,4 @@
-const initialCards = [
+export const initialCards = [
     {
       name: "Калининград",
       link: "https://images.unsplash.com/photo-1629104411440-7ebc9134f93e?q=80&w=3687&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
